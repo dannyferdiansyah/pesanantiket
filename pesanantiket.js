59f5e0dff96f684b1607199b5ac465f1a0728cfe
dannyferdiansyah <dannyferdiansyah389@gmail.com>
2022-06-30 15:28:40 +0700

    Code repo

--- pesanantiket.js
+++ pesanantiket.js
@@ -5,18 +5,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    const pesanantiket = [
-        {
-            "id": "1",
-            "image": "https://s2.rdbuz.com/buslogos/country/IDN/logo/18107.png",
-            "jam": "11:07:00",
-            "harga": "270.000",
-            "estimasi": "32",
-            "namabis": "Sinar Jaya",
-            "tipebis": "Executive Class (2+2)",
-            "tujuan": "Terminal Bungurasih - Terminal Kampung Rambutan"
-        }
-    ];
+    // const pesanantiket = [
+    //     {
+    //         "id": "1",
+    //         "image": "https://s2.rdbuz.com/buslogos/country/IDN/logo/18107.png",
+    //         "jam": "11:07:00",
+    //         "harga": "270.000",
+    //         "estimasi": "32",
+    //         "namabis": "Sinar Jaya",
+    //         "tipebis": "Executive Class (2+2)",
+    //         "tujuan": "Terminal Bungurasih - Terminal Kampung Rambutan"
+    //     }
+    // ];
     res.send("Hello Quotes!");
     res.status(200).json(pesanantiket["id"]);
     print("hadeh");
